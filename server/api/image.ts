9a984292195a8f191aa7af323084dcc74bb6a1d2
IPT-S01 <ipt-s01@IPT-S01s-Mini.infopeaktech.local>
2024-08-23 10:39:57 +0800

    test

--- server/api/image.ts
+++ server/api/image.ts
@@ -4,13 +4,30 @@ import path from 'path'
 export default defineEventHandler((event) => {
   try {
     console.log(process.cwd());
-    const config: any = useRuntimeConfig()
+    // const config: any = useRuntimeConfig()
 
-    const tempDir = path.join(config.public.tempDir, 'temp')
-    console.log(tempDir, 'tempDir');
+    // const tempDir = path.join(config.public.tempDir, 'temp')
+    // console.log(tempDir, 'tempDir');
+
+    // const files = fs.readdirSync(tempDir)
+    // console.log(files, 'files');
+
+    // const images = files.filter(file => {
+    //   const ext = path.extname(file).toLowerCase()
+    //   return ['.jpg', '.jpeg', '.png', '.gif'].includes(ext)
+    // })
+    // return images
+    const rootDir = process.cwd()
+    const tempDir = path.join(rootDir, 'public', 'temp')
+    console.log('Attempting to read from:', tempDir);
+
+    if (!fs.existsSync(tempDir)) {
+      console.error('Directory does not exist:', tempDir);
+      return [];
+    }
 
     const files = fs.readdirSync(tempDir)
-    console.log(files, 'files');
+    console.log('Files found:', files);
 
     const images = files.filter(file => {
       const ext = path.extname(file).toLowerCase()
